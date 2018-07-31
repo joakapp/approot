c6fbecd22ad2dd76b36563a6cb50064ae0864849
lushaonan <lushaonany@126.com>
2018-07-31 14:07:25 +0800

    初始化资源信息变更

---
+++ db-core/src/main/java/com/ms/aspect/db/DynamicDataSourceAspect.java
@@ -0,0 +1,17 @@
+package com.ms.aspect.db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * 动态数据源拦截处理
+ * @Author lushaonan
+ */
+@Slf4j
+@Aspect
+public class DynamicDataSourceAspect {
+
+
+
+
+}
